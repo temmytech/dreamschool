69995f7a7c4baa04ab91e8df54bc122b8e257972
temmytech <temitopeoluwamuyiwa@gmail.com>
2022-02-21 06:47:41 +0100

    java script page

---
+++ script.js
@@ -0,0 +1,12 @@
+// <!-- -----Javascript for toogle----- -->
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+    navLinks.style.display = "block";
+}
+
+function hideMenu() {
+    navLinks.style.display = "none ";
+}
